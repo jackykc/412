b6afe7c8617714ea9c28265d638f1eeeffed34c8
Yi <yi16@ualberta.ca>
2019-03-21 12:17:18 -0600

    .

--- catkin_ws/src/demo5/src/demo6.py
+++ catkin_ws/src/demo5/src/demo6.py
@@ -9,10 +9,10 @@ from ar_track_alvar_msgs.msg import AlvarMarkers
 import tf
 import numpy as np
 from ros_numpy import numpify, geometry
-
+from image_geometry import PinholeCameraModel
 from tf.transformations import decompose_matrix, euler_from_quaternion
 
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 import cv2
 import cv_bridge
 
@@ -27,14 +27,20 @@ dist = np.array([0.01429000810948565, -0.08328252729130574, 0.000349059108412665
 bridge = cv_bridge.CvBridge()
 
 
+
 def draw(img, corner, imgpts):
     img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
     img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
     img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
     return img
 
-global np_current_marker_pose
+global np_current_marker_pose, caminfo
 np_current_marker_pose = None
+
+def caminfo_callback(msg):
+    global caminfo
+    caminfo = msg
+
 def marker_cb(msg):
     global np_current_marker_pose
     if len(msg.markers):
@@ -42,24 +48,41 @@ def marker_cb(msg):
 
         for marker in msg.markers:
             current_marker_pose = marker.pose.pose
-            np_current_marker_pose = numpify(current_marker_pose)
-
+            # np_current_marker_pose = numpify(current_marker_pose)
+            # print current_marker_pose
+            np_current_marker_pose = (current_marker_pose.position.x, current_marker_pose.position.y, current_marker_pose.position.z)
+            np_current_marker_pose = tf.transformations.euler_from_quaternion(np_current_marker_pose)
 
 def img_callback(msg):
-    global np_current_marker_pose
+
+    global np_current_marker_pose, caminfo
+    # print np_current_marker_pose
     img = bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
     if np_current_marker_pose is not None:
-        scale, shear, rpy_angles, translation_vector, perspective = tf.transformations.decompose_matrix(np_current_marker_pose)
-        print rpy_angles, translation_vector
+        cam_model = PinholeCameraModel()
+        cam_model.fromCameraInfo(caminfo)
+        try:
+            (trans,rot) = listener.lookupTransform(cam_model.tfFrame(), '/base_link', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logerr("error")
+            return
+        print 'trans'
+        print trans
+        pixels = cam_model.project3dToPixel(np_current_marker_pose)
+        # pixels = cam_model.project3dToPixel(trans)
+        print 'pixels'
+        print pixels
+        # cv2.circle(img, (int(u), int(v)), 20, (0,255,0), -1)
         
-        imgpts, jac = cv2.projectPoints(axis, np.asarray(rpy_angles), np.asarray(translation_vector), mtx, dist)
+        # scale, shear, rpy_angles, translation_vector, perspective = tf.transformations.decompose_matrix(np_current_marker_pose)
+        # print rpy_angles, translation_vector
+        
+        # imgpts, jac = cv2.projectPoints(axis, np.asarray(rpy_angles), np.asarray(translation_vector), mtx, dist)
 
 
-        img = draw(img,(20, 20),imgpts)
+        # img = draw(img,(20, 20),imgpts)
 
-        img_pub.publish(bridge.cv2_to_imgmsg(img, encoding='bgr8'))
+    img_pub.publish(bridge.cv2_to_imgmsg(img, encoding='bgr8'))
 '''
 scale, shear, rpy_angles, translation_vector, perspective = tf.transformations.decompose_matrix(np_current_marker_pose)
             print rpy_angles, translation_vector
@@ -72,7 +95,8 @@ scale, shear, rpy_angles, translation_vector, perspective = tf.transformations.d
 rospy.init_node('patrol')
 
 marker_sub = rospy.Subscriber('/ar_pose_marker', AlvarMarkers, marker_cb)
-img_sub = rospy.Subscriber('/camera/rgb/image_rect_color', Image, img_callback)
+img_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, img_callback)
 img_pub = rospy.Publisher('transformed_img', Image, queue_size=1)
-
+caminfo_sub = rospy.Subscriber('/camera/rgb/camera_info', CameraInfo, caminfo_callback)
+listener = tf.TransformListener()
 rospy.spin()
---
+++ catkin_ws/src/demo5/src/fetchbox.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from ar_track_alvar_msgs.msg import AlvarMarkers
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+# from odom_msgs.msg import 
+import actionlib
+import tf
+import cv2, cv_bridge
+import numpy as np
+import smach
+import smach_ros
+
+from tf.transformations import decompose_matrix, euler_from_quaternion
+from ros_numpy import numpify, geometry
+
+global current_marker_pose
+global current_pose
+global initial_pose
+global finished_marker_ids
+global current_marker_id
+current_marker_id = None
+finished_marker_ids = []
+current_marker_pose = None
+current_pose = None
+initial_pose = None
+
+def odom_callback(msg):
+    global current_pose
+    global initial_pose
+    current_pose = msg.pose.pose
+    if initial_pose is None:
+        initial_pose = current_pose
+def img_callback(msg):
+    return
+
+def marker_cb(msg):
+    global current_marker_pose
+    global finished_marker_ids
+    global current_marker_id
+    # print msg.markers
+    if len(msg.markers):
+        for marker in msg.markers:
+            if marker.id in finished_marker_ids:
+                continue
+            if current_marker_id is None:
+                 current_marker_id = marker.id
+            current_marker_pose = marker.pose.pose
+        # print current_marker_pose
+
+rospy.init_node('part2')
+img_sub = rospy.Subscriber('/camera/rgb/image_raw', Image, img_callback)
+# img_pub = rospy.Publisher('transformed_img', Image, queue_size=1)
+odom_sub = rospy.Subscriber("/odom", Odometry, odom_callback)
+marker_sub = rospy.Subscriber('/ar_pose_marker', AlvarMarkers, marker_cb)
+cmd_vel_pub = rospy.Publisher("cmd_vel_mux/input/teleop", Twist, queue_size=1)
+
+client = actionlib.SimpleActionClient('move_base', MoveBaseAction)  # <3>
+client.wait_for_server()
+
+class Turn(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['servo'])
+        self.twist = Twist()
+        self.twist.linear.x = 0
+        self.twist.angular.z = 0.4
+        
+    def execute(self, data):
+        global finished_marker_ids
+        global current_marker_id
+        global current_marker_pose
+        current_marker_id = None
+        current_marker_pose = None
+        while not rospy.is_shutdown():
+            cmd_vel_pub.publish(self.twist)
+            if current_marker_pose is not None:
+                return 'servo'
+
+class VisualServo(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['navigate', 'start'])
+        self.twist = Twist()
+        print "init"
+    def execute(self, data):
+        global current_marker_pose
+        # rospy.loginfo("VisualServo")
+        print "start"
+        reached = False
+        while not rospy.is_shutdown():
+            # print current_marker_pose
+            if (current_marker_pose is not None) and (current_marker_pose.position.x > 0.7):
+                print current_marker_pose.position.x
+                self.twist.linear.x = 0.1
+                direction = current_marker_pose.position.y > 0
+                if direction: 
+                    direction = 1
+                else:
+                    direction = -1
+                self.twist.angular.z = direction * 0.3
+                cmd_vel_pub.publish(self.twist)
+                reached = True
+            elif reached:
+                return 'navigate'
+        return 'navigate'
+
+class NavigateGoal(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['start', 'finish'])
+        self.twist = Twist()
+        print "init"
+    def execute(self, data):
+        global current_marker_pose
+        global current_pose
+        # rospy.loginfo("VisualServo")
+        print "navigate"
+        
+        print current_marker_pose
+        print current_pose 
+        if (current_marker_pose is not None and current_pose is not None):
+            np_current_pose = numpify(current_pose)
+            np_current_marker_pose = numpify(current_marker_pose)
+            np_goal_pose = np.matmul(np_current_pose, np_current_marker_pose)
+            temp_goal_pose = geometry.numpy_to_pose(np_goal_pose)
+            temp_goal_pose.position.x += 1.05
+
+            print temp_goal_pose
+            goal_pose = MoveBaseGoal()
+            goal_pose.target_pose.header.frame_id = 'odom'
+            goal_pose.target_pose.pose = temp_goal_pose
+            goal_pose.target_pose.pose.position.z = 0.0
+            goal_pose.target_pose.pose.orientation.x = 0#current_marker_pose.orientation.x
+            goal_pose.target_pose.pose.orientation.y = 0#current_marker_pose.orientation.y
+            goal_pose.target_pose.pose.orientation.z = current_pose.orientation.z
+            goal_pose.target_pose.pose.orientation.w = current_pose.orientation.w
+
+
+            '''
+            goal_pose.target_pose.pose.position.x = np_goal_pose[0][3]
+            goal_pose.target_pose.pose.position.y = np_goal_pose[1][3]
+            goal_pose.target_pose.pose.position.z = 0.0
+            goal_pose.target_pose.pose.orientation.x = 0#current_marker_pose.orientation.x
+            goal_pose.target_pose.pose.orientation.y = 0#current_marker_pose.orientation.y
+            goal_pose.target_pose.pose.orientation.z = current_pose.orientation.z
+            goal_pose.target_pose.pose.orientation.w = current_pose.orientation.w
+            '''
+
+            # print np_goal_pose
+            # print goal_pose
+            # print current_pose
+            global finished_marker_ids
+            global current_marker_id
+            client.send_goal(goal_pose)
+            client.wait_for_result()
+
+        return 'finish'
+
+class NavigateStart(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['turn', 'finish'])
+    def execute(self, data):
+        global current_marker_pose
+        global current_pose
+        global initial_pose
+        print "start"
+        
+        if (current_marker_pose is not None and current_pose is not None):
+            goal_pose = MoveBaseGoal()
+            goal_pose.target_pose.header.frame_id = 'odom'
+            goal_pose.target_pose.pose = initial_pose
+            global finished_marker_ids
+            global current_marker_id
+            print finished_marker_ids
+            print current_marker_id
+            client.send_goal(goal_pose)
+            client.wait_for_result()
+            finished_marker_ids.append(current_marker_id)
+            current_marker_id = None
+
+        return 'turn'
+
+
+# follower = Follower()
+sm = smach.StateMachine(outcomes=['finish'])
+with sm:
+    # Add states to the container
+    smach.StateMachine.add('TURN', Turn(), 
+                 transitions={'servo':'GOAL'})
+    smach.StateMachine.add('SERVO', VisualServo(), 
+                 transitions={'navigate':'GOAL', 'start': 'START'})
+    smach.StateMachine.add('GOAL', NavigateGoal(), 
+                 transitions={'start':'START', 'finish':'finish'})
+    smach.StateMachine.add('START', NavigateStart(), 
+                 transitions={'turn':'TURN', "finish": "finish"})
+    # smach.StateMachine.add('TASK1', Task1(), 
+    #              transitions={'go':'GO'})
+    # smach.StateMachine.add('TASK2', Task2(), 
+    #              transitions={'go':'GO'})
+    # smach.StateMachine.add('TASK3', Task3(), 
+    #              transitions={'go':'GO'})
+
+# Create and start the introspection server
+# sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+# sis.start()
+
+outcome = sm.execute()
+rospy.spin()
+# sis.stop()
